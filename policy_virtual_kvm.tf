138279cb417bcfa50a7e7265e98cee4d8177c9b7
jerewill-cisco <73540564+jerewill-cisco@users.noreply.github.com>
2022-02-22 15:53:30 -0500

    updated virtual kvm policy to add tunneled kvm

--- policy_virtual_kvm.tf
+++ policy_virtual_kvm.tf
@@ -10,4 +10,9 @@ resource "intersight_kvm_policy" "default" {
   remote_port               = 2068
   enable_video_encryption   = true
   enable_local_server_video = true
+
+  ## This really should be a native attribute like this...
+  # tunneled_kvm_enabled      = true 
+  ## But it's not yet... so use additional_properties instead.
+  additional_properties = jsonencode({ TunneledKvmEnabled = true })
 }
