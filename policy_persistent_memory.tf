666a91c5e435ef5273a3e379c9391a5a31f11775
jerewill-cisco <73540564+jerewill-cisco@users.noreply.github.com>
2022-02-28 16:54:06 -0500

    removed application of persistent memory policy which appears broken

--- policy_persistent_memory.tf
+++ policy_persistent_memory.tf
@@ -1,3 +1,5 @@
+# This resource appears to be broken
+# https://github.com/CiscoDevNet/terraform-provider-intersight/issues/180
 resource "intersight_memory_persistent_memory_policy" "configured_from_os" {
   name = "configured_from_os"
   tags = [local.terraform]
