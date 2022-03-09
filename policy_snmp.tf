ff21369d86425d44ed0d639617fbfb333158999c
jerewill-cisco <73540564+jerewill-cisco@users.noreply.github.com>
2022-03-09 16:08:24 -0500

    add example for snmpv3

--- policy_snmp.tf
+++ policy_snmp.tf
@@ -52,3 +52,36 @@ resource "intersight_snmp_policy" "snmpv2c" {
   sys_contact             = "itops@email.com"
   sys_location            = "SJC-Building999"
 }
+
+resource "intersight_snmp_policy" "snmpv3" {
+  name = "snmpv3"
+  tags = [local.terraform]
+  organization {
+    moid = local.organization
+  }
+
+  v2_enabled   = false
+  v3_enabled   = true
+  enabled      = true
+  snmp_port    = 161
+  sys_contact  = "itops@email.com"
+  sys_location = "SJC-Building999"
+
+  snmp_users {
+    name             = "ucs"
+    auth_type        = "SHA"
+    privacy_type     = "AES"
+    security_level   = "AuthPriv"
+    auth_password    = var.imc_local_admin_password
+    privacy_password = var.imc_local_admin_password
+  }
+
+  snmp_traps {
+    enabled     = true
+    nr_version  = "V3"
+    user        = "ucs" # must be defined in an snmp_users block
+    type        = "Trap"
+    destination = "snmptraps.example.com"
+    port        = 162
+  }
+}
