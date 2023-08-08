ac882d9982f24cdc654856d32bba4629dcae2ef0
Jeremy Williams <73540564+jerewill-cisco@users.noreply.github.com>
2023-08-08 09:29:43 -0400

    Added 'recommended' policy example

--- policy_server_firmware.tf
+++ policy_server_firmware.tf
@@ -1,4 +1,4 @@
-
+# This firmware policy statically sets the desired versions
 resource "intersight_firmware_policy" "August2023" {
   name = "August2023"
   dynamic "tags" {
@@ -22,4 +22,35 @@ resource "intersight_firmware_policy" "August2023" {
     model_family   = "UCSC-C220-M5"
     bundle_version = "4.2(3e)"
   }
+}
+
+# This data resource and firmware policy dynamically creates a set of firmware policies
+# for each server family with the version set to the recommended version
+data "intersight_firmware_distributable" "recommended" {
+  recommended_build = "Y"
+  import_state      = "Imported"
+  image_type        = "Intersight Managed Mode Server"
+}
+
+resource "intersight_firmware_policy" "recommended" {
+  for_each = { for i in data.intersight_firmware_distributable.recommended.results : "${i.moid}" => i }
+  name     = each.value.mdfid
+  dynamic "tags" {
+    for_each = local.tags
+    content {
+      key   = tags.key
+      value = tags.value
+    }
+  }
+  organization {
+    moid = local.organization
+  }
+
+  target_platform = "FIAttached"
+
+  model_bundle_combo {
+    model_family   = trimsuffix(trimsuffix(trimsuffix(each.value.supported_models[0], "X"), "N"), "S")
+    bundle_version = each.value.nr_version
+  }
+
 }
